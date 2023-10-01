6c4bce8281795445c805cccbf8775e870b8fc075
Isabelle Blixt <108222104+Bella0g@users.noreply.github.com>
2023-10-01 16:08:34 +0200

    added basic chat structure

---
+++ client/src/Chat.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Chat({socket, usename, room}) {
+  return (
+    <div class="container" className='chat-window'>
+          <div className="chat-header">
+
+          </div>
+          <div className="chat-body">
+
+          </div>
+          <div className="chat-footer">
+
+          </div>
+    </div>
+  )
+}
+
+export default Chat
