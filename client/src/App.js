812454f5d28c38649bb7443ab2b0040061eb4d84
Isabelle Blixt <108222104+Bella0g@users.noreply.github.com>
2023-10-01 15:52:22 +0200

    added chat.js file and declared constants

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
   return (
     <div className="App">
       
