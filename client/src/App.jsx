f605f50ce7a706dc8158137703c90d21afa747ee
Isabelle Blixt <108222104+Bella0g@users.noreply.github.com>
2023-10-02 13:29:48 +0200

    added comments to App.jsx will change later

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,25 +3,29 @@ import Chat from "./Chat";
 import io from "socket.io-client";
 import {  createContext, useState } from "react";
 import ReactSwitch from "react-switch";
-import { BrowserRouter as Router, Route, link } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, link } from 'react-router-dom';
 
-// add routing to switch between join and chat
-// add global state and context with theme
+// Create a socket connection
 const socket = io.connect("http://localhost:3001");
 
+// Create a context to manage the theme globally
 export const ThemeContext = createContext(null);
 
 function App() {
-
+  // State for managing the theme
   const [theme, setTheme] = useState("dark");
+
+  // Function to toggle between light and dark themes
   const toggleTheme = () => {
     setTheme((curr) => (curr === "light" ? "dark" : "light"));
   };
 
+   // State for managing username, room, and chat display
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
   const [showChat, setShowChat] = useState(false);
 
+  // Function to join a chat room
   const joinRoom = () => {
     if (username !== "" && room !== "") {
       socket.emit("join_room", room);
@@ -30,14 +34,18 @@ function App() {
   };
 
   return (
+     // Provide the theme and theme toggling function to the context
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
     <div className="App" id={theme}>
         <div className="switch">
           <label> {theme === "light" ? "Light" : "Dark"}</label>
           <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
         </div>
+        
+      {/* Conditional rendering based on whether to show the chat or join screen */}
       {!showChat ? (
         <div class="container" className="joinChatContainer">
+            {/* UI for joining a chat room */}
           <div className='img-container'>
             <img src="./chat-logo.png" alt="" />
           </div>
@@ -59,6 +67,7 @@ function App() {
           <button onClick={joinRoom}>Join A Room</button>
         </div>
       ) : (
+         // Render the Chat component with the provided socket, username, and room
         <Chat socket={socket} username={username} room={room} />
       )}
     </div>
