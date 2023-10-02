9818cd144d4b022cc5f8aa5516147b3da94c3b99
Isabelle Blixt <108222104+Bella0g@users.noreply.github.com>
2023-10-02 16:20:16 +0200

    added routes insted of state to join room

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,54 @@ import Chat from "./Chat";
 import io from "socket.io-client";
 import {  createContext, useState } from "react";
 import ReactSwitch from "react-switch";
-// import { BrowserRouter as Router, Route, link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
 // Create a socket connection
 const socket = io.connect("http://localhost:3001");
 
 // Create a context to manage the theme globally
 export const ThemeContext = createContext(null);
+
+function JoinChat() {
+  // State for managing username and room
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
+  // Function to join a chat room
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+
+  return (
+    <div className="joinChatContainer">
+      {/* UI for joining a chat room */}
+      <div className='img-container'>
+        <img src="./chat-logo.png" alt="" />
+      </div>
+      <h3>Chat Lounge</h3>
+      <input
+        type="text"
+        placeholder="User name.."
+        onChange={(event) => {
+          setUsername(event.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Room ID..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <Link to={`/chat/${username}/${room}`}>
+        <button onClick={joinRoom}>Join A Room</button>
+      </Link>
+    </div>
+  );
+}
+
 
 function App() {
   // State for managing the theme
@@ -20,21 +61,7 @@ function App() {
     setTheme((curr) => (curr === "light" ? "dark" : "light"));
   };
 
-   // State for managing username, room, and chat display
-  const [username, setUsername] = useState("");
-  const [room, setRoom] = useState("");
-  const [showChat, setShowChat] = useState(false);
-
-  // Function to join a chat room
-  const joinRoom = () => {
-    if (username !== "" && room !== "") {
-      socket.emit("join_room", room);
-      setShowChat(true);
-    }
-  };
-
   return (
-     // Provide the theme and theme toggling function to the context
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
     <div className="App" id={theme}>
         <div className="switch">
@@ -42,35 +69,14 @@ function App() {
           <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
         </div>
         
-      {/* Conditional rendering based on whether to show the chat or join screen */}
-      {!showChat ? (
-        <div class="container" className="joinChatContainer">
-            {/* UI for joining a chat room */}
-          <div className='img-container'>
-            <img src="./chat-logo.png" alt="" />
-          </div>
-          <h3>Chat Lounge</h3>
-          <input
-            type="text"
-            placeholder="User name.."
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
-          <input
-            type="text"
-            placeholder="Room ID..."
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          />
-          <button onClick={joinRoom}>Join A Room</button>
-        </div>
-      ) : (
-         // Render the Chat component with the provided socket, username, and room
-        <Chat socket={socket} username={username} room={room} />
-      )}
-    </div>
+        {/* Routes */}
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<JoinChat />} />
+            <Route path="/chat/:username/:room" element={<Chat socket={socket} />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </ThemeContext.Provider>
   );
 }
